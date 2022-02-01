e7a882c627722e4724e1b8ed780091329dbae50e
kristoffer <kristnyv@stud.ntnu.no>
2022-02-01 16:14:53 +0100

    task 1 minus flip

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -8,26 +8,50 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture texture;
+	float speedX = 3f;
+	float speedY = 3f;
+	int x;
+	int y;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		texture = new Texture("attackhelicopter.png");
+		x = Gdx.graphics.getWidth()/2;
+		y = Gdx.graphics.getHeight()/2;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		//When the ball's x position is on either side of the screen.
+		//The width of the sprite is taken into account.
+		if (x > Gdx.graphics.getWidth() - texture.getWidth() / 2 || x < 0 + texture.getWidth() / 2) {
+			//Here we flip the speed, so it bonces the other way.
+			speedX = -speedX;
+		}
+		//Same as above, but with on the y-axis.
+		if (y > Gdx.graphics.getHeight() - texture.getHeight() / 2 || y < 0 + texture.getHeight() / 2) {
+			speedY = -speedY;
+		}
+
+		//Move the ball according to the speed.
+		x += speedX;
+		y += speedY;
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//Draw the ball so the center is at x and y. Normally it would be drawn from the lower left corner.
+		batch.draw(texture, x - texture.getWidth() / 2, y - texture.getHeight() / 2);
 		batch.end();
 	}
-	
+
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		texture.dispose();
 	}
 }
